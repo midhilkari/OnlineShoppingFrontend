3a2002a52c9ecfa8e7e768527c5599be3ca2fe4e
skari <midhilkari@gmail.com>
2020-05-29 11:24:36 +1000

    Initial commit

---
+++ src/auth/helper/index.js
@@ -0,0 +1,66 @@
+import { API } from "../../backend";
+//API means http://localhost:8000/api/
+
+export const signup = (user) => {
+  console.log(user);
+  const URL = "http://localhost:8000/api/signup";
+  return fetch(URL, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const signin = (user) => {
+  return fetch(`${API}signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const authenticate = (data, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
+
+export const signout = (next) => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("jwt");
+    next();
+
+    return fetch(`${API}signout`, {
+      method: "GET",
+    })
+      .then((response) => console.log("Signout success"))
+      .catch((err) => console.log(err));
+  }
+};
+
+//to validate user is signed in or out
+export const isAuthenticated = () => {
+  if (typeof window !== "undefined") {
+    return false;
+  }
+  if (localStorage.getItem("JWT")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
---
+++ src/core/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles.css";
+//import { API } from "../backend";
+import { API } from "./../backend";
+import Base from "./Base";
+
+export default function Home() {
+  console.log("API IS", API);
+
+  return (
+    <Base title="Home Page" description="Welcome to the  Tshirt store">
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">Test</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">Test</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">Test</button>
+        </div>
+      </div>
+    </Base>
+  );
+}
